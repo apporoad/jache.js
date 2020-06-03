d9636544afc1fe162c2801f3b59b162b08fb4805
apporoad <apporoadina@163.com>
2020-06-03 15:29:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const {checkEqual} = require('check-equal')
+
+function abc(){}
+
+function c(){}
+
+
+console.log(checkEqual(c,c))
+
+
+
+exports.cache = (fn , options) =>{
+
+}
+
+//https://blog.csdn.net/weixin_30929195/article/details/98333586
---
+++ test.js
@@ -0,0 +1,5 @@
+
+var abc = 'asdfsdf'
+exports.test=()=>{
+    console.log(abc)
+}
